27c3495a3a567889ec9fcd47998dcc6f1b658bc3
Seu Nome <andreifurtadodutra@gmail.com>
2023-11-30 20:03:23 -0300

    validar dados inseridos pelo usuário para criação de conta

--- src/index.js
+++ src/index.js
@@ -7,3 +7,17 @@ app.get("/", (request, response) => {
 });
 
 app.listen(8080, () => console.log("Servidor iniciado"));
+
+function validarDados(request, response, next) {
+  const name = request.body.name;
+  const email = request.body.email;
+  const password = request.body.password;
+
+  if (!name || !email || !password) {
+    return response.status(401).json("Por favor, preencha todos os campos.");
+  } else {
+    next();
+  }
+}
+
+let users = [];
